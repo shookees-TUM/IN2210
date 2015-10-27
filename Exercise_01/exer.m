3128795f974e5ee708b3095606028d34260dbec6
Paulius Šukys <paul.sukys@gmail.com>
2015-10-27 23:11:48 +0100

    Slight modifications for general testing on convolution

--- Exercise_01/exer.m
+++ Exercise_01/exer.m
@@ -1,7 +1,8 @@
 H = ones(3, 3);
-H(2, 2) = -8;
-I = imread('lena.gif');
-J = convoluteImage(H, I, 'mirror');
+H = H./9;
+%H(2, 2) = -8;
+I = imread('test.gif');
+J = convoluteImage(H, I, 'multiply');
 colormap(gray(256)); 
 image(J);
 %pause; 
