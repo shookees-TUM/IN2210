6d2616e986ff776620c6e7524acdb3db2b1e7207
Paulius Šukys <paul.sukys@gmail.com>
2015-10-27 23:08:43 +0100

    Fixes for convolution.m: 1. Removed global change to image_size to leave it as I's size; moved the padded size to padded_image size directly 2. for string comparisson used strcmp instead of duck typing (which apparently doesn't work) 3. Implemented multiply solution 4.Reduced the complexity of convolution cycles and removed the need for cropping later

--- Exercise_01/convolution.m
+++ Exercise_01/convolution.m
@@ -9,12 +9,11 @@ function J = convolution(H, I, border)
     % Strictly defining that convolution is executed on 2 dimension layer
     image_size = image_size(1:2);
     % Adding respectively to vector
-    image_size = image_size + pad * 2;
-    padded_image = zeros(image_size);
+    padded_image = zeros(image_size + pad * 2);
     % Copy image, with appropriate padding
     padded_image(1 + pad(1): end - pad(1), 1 + pad(2): end - pad(2)) = I;
     % Handle border problem
-    if border == 'mirror'
+    if strcmp(border, 'mirror') == 1
         %left border
         padded_image(1 + pad(1): end - pad(1), 1 : pad(2)) = fliplr(padded_image(1 + pad(1): end - pad(1), 1 + pad(2): 2 * pad(2)));
         %top left diagonal space
@@ -31,26 +30,34 @@ function J = convolution(H, I, border)
         padded_image(end - pad(1) + 1: end, 1 + pad(2): end - pad(2)) = flipud(padded_image(end - 2 * pad(1) + 1: end - pad(1), 1 + pad(2): end - pad(2)));
         %bottom left diagonal space
         padded_image(end - pad(1) + 1: end, 1: pad(2)) = fliplr(flipud(padded_image(end - 2 * pad(1) + 1: end - pad(1), pad(2) + 1: 2 * pad(2))));
-    elseif border == 'multiply'
+    elseif strcmp(border, 'multiply') == 1
+        % The general approach is to take whole line and multiply by ones vector to get a matrix
         %left border
+        padded_image(1 + pad(1): end - pad(1), 1 : pad(2)) = padded_image(1 + pad(1): end - pad(1), 1 + pad(2): 1 + pad(2)) * ones(1, pad(2));
         %top left diagonal space
+        padded_image(1: pad(1), 1: pad(2)) = padded_image(1 + pad(1), 1 + pad(2)) * ones(pad);
         %top border
+        padded_image(1: pad(1), 1 + pad(2): end - pad(2)) = ones(pad(1), 1) * padded_image(1 + pad(1): 1 + pad(1), 1 + pad(2): end - pad(2));
         %top right diagonal space
+        padded_image(1: pad(1), end - pad(2) + 1: end) = padded_image(1 + pad(1), 1 + pad(2)) * ones(pad);
         %right border
+        padded_image(1 + pad(1): end - pad(1), end - pad(2) + 1: end) = padded_image(1 + pad(1): end - pad(1), end - pad(2): end - pad(2)) * ones(1, pad(2));
         %bottom right diagonal space
+        padded_image(end - pad(1) + 1: end , end - pad(2) + 1: end) = padded_image(end - pad(1), end - pad(2)) * ones(pad);
         %bottom border
+        padded_image(end - pad(1) + 1: end, 1 + pad(2): end - pad(2)) = ones(pad(1), 1) * padded_image(end - pad(1): end - pad(1), 1 + pad(2): end - pad(2));
         %bottom left diagonal space
+        padded_image(end - pad(1) + 1: end, 1: pad(2)) = padded_image(end - pad(1), 1 + pad(2)) * ones(pad);
     end %if none is chosen, simply leave as it is
-    % Initiate resulting image, leaving pads (comfy for indexing)
+
+    % Initiate resulting image
     J = zeros(size(image_size));
-    for i = 1 + pad(1): image_size(1) - pad(1)
-        for j = 1 + pad(2): image_size(2) - pad(2)
+    for i = 1: image_size(1)
+        for j = 1: image_size(2)
             % Assign result the elementwise multiplication's sum
-            applied_mask = H.*padded_image(i - pad(1): i + pad(1), j - pad(2): j + pad(2));
+            applied_mask = H.*padded_image(1 + i - pad(1): 1 + i + pad(1), 1 + j - pad(2): 1 + j + pad(2));
             % sum adds up columns, : makes it sum to 
             J(i, j) = sum(applied_mask(:));
         end
     end
-    % Remove padding
-    J = J(1 + pad(1): end - pad(1), 1 + pad(2): end - pad(2));
 end
