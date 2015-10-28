5a20f368f9c57a1afeecbdb396ecbaacae387184
Paulius Šukys <paul.sukys@gmail.com>
2015-10-28 09:16:41 +0100

    Pretified the outputs for 1st exercise - added a plot for comparisson

--- Exercise_01/exer.m
+++ Exercise_01/exer.m
@@ -1,8 +1,25 @@
-H = ones(3, 3);
+% Exercise 1
+H = ones(3);
+% Seems that H./9 was meant, as it is the case in the theory slides
 H = H./9;
-%H(2, 2) = -8;
-I = imread('test.gif');
+I = imread('lena.gif');
 J = convoluteImage(H, I, 'multiply');
+figure
 colormap(gray(256)); 
+% Original image
+subplot(1, 2, 1);
+image(I);
+daspect ([1 1]); % Keeps aspect ratio 1:1
+% Convoluted image
+subplot(1, 2, 2);
 image(J);
-%pause; 
+daspect ([1 1]); % Keeps aspect ratio 1:1
+pause; 
+% End of Exercise 1
+
+% Exercise 2
+sigma = 3;
+dimGauss = 2;
+mask_size = 3 * sigma;
+G2D = gaussian2D(mask_size, sigma);
+
