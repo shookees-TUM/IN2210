417c811d37a31a1ef83e4820369acb2f8a46c2e6
Paulius Šukys <paul.sukys@gmail.com>
2015-10-28 23:57:32 +0100

    Implemented code from exer_03, also added the last exercise 3 case (c).

--- Exercise_01/exer.m
+++ Exercise_01/exer.m
@@ -9,12 +9,12 @@ colormap(gray(256));
 % Original image
 subplot(1, 2, 1);
 image(I);
-title("Original");
+title('Original');
 daspect([1 1]); % Keeps aspect ratio 1:1
 % Convoluted image
 subplot(1, 2, 2);
 image(J);
-title("Convoluted");
+title('Convoluted');
 daspect([1 1]); % Keeps aspect ratio 1:1
 %pause; 
 % End of Exercise 1
@@ -55,6 +55,7 @@ I_G1D_sigma3 = convoluteImage(G1D_sigma3', I_G1D_sigma3, 'multiply');
 I_G1D_sigma3_time = toc;
 
 figure('Name', 'Exercise 2.a');
+colormap(gray(256)); 
 subplot(2, 2, 1);
 image(I_G2D_sigma1);
 title('Sigma = 1 Gaussian 2D');
@@ -74,11 +75,12 @@ daspect([1 1]);
 %pause;
 
 % Squared differences
-diff_sigma1 = I_G2D_sigma1 - I_G1D_sigma1
-%diff_sigma1 = diff_sigma1.^2;
-diff_sigma3 = I_G2D_sigma3 - I_G1D_sigma3
-%diff_sigma3 = diff_sigma3.^2;
+diff_sigma1 = I_G2D_sigma1 - I_G1D_sigma1;
+diff_sigma1 = diff_sigma1.^2;
+diff_sigma3 = I_G2D_sigma3 - I_G1D_sigma3;
+diff_sigma3 = diff_sigma3.^2;
 figure('Name', 'Exercise 2.b');
+colormap(gray(256)); 
 subplot(1, 2, 1);
 image(diff_sigma1);
 title('Sigma = 1 difference');
@@ -87,9 +89,65 @@ subplot(1, 2, 2);
 image(diff_sigma3);
 title('Sigma = 3 difference');
 daspect([1 1]);
-%pause;
 
 % Time differences
 printf('Exercise 2.c\n');
 printf('2D sigma took:\n %d s for sigma = 1\n %d s for sigma = 3\n', I_G2D_sigma1_time, I_G2D_sigma3_time);
 printf('2 times 1D sigma took:\n %d s for sigma = 1\n %d s for sigma = 3\n', I_G1D_sigma1_time, I_G1D_sigma3_time);
+% End of Exercise 2
+% Exercise 3
+
+% define basic grandient filters
+Dx = [-1, 0, 1;
+      -1, 0, 1;
+      -1, 0, 1];
+Dy = transpose(Dx);
+
+% compute x gradient image
+GradX = convoluteImage(Dx, I, 'multiply');
+% compute y gradient image
+GradY = convoluteImage(Dy, I, 'multiply');
+figure('Name', 'Exercise 3.a');
+colormap(gray(256)); 
+subplot(1, 2, 1);
+image(GradX);
+title('x-direction gradient image');
+daspect([1 1]);
+subplot(1, 2, 2);
+image(GradY);
+title('y-direction gradient image');
+daspect([1 1]);
+
+% gradient magnitude
+grad_I = GradX.^2 + GradY.^2;
+grad_I = grad_I.^0.5;
+% gradient orientation
+Psi_grad_I = atan2(Grady, Gradx);
+figure('Name', 'Exercise 3.b');
+colormap(gray(256));
+subplot(1, 2, 1);
+image(grad_I);
+title('Gradient magnitude');
+daspect([1 1]);
+subplot(1, 2, 2);
+image(Psi_grad_I);
+title('Gradient orientation');
+daspect([1 1]);
+
+% Use associativity law for I_x = D_x * (G * I)
+% To get I_x = (D_x * G) * I
+cIx_kernel = convolution(G2D_sigma1, Dx, 'multiply');
+cIx = convoluteImage(cIx_kernel, I, 'multiply');
+cIy_kernel = convolution(G2D_sigma1, Dy, 'multiply');
+cIy = convoluteImage(cIy_kernel, I, 'multiply');
+
+figure('Name', 'Exercise 3.c');
+colormap(gray(256));
+subplot(1, 2, 1);
+image(cIx);
+title('Noise reduction x-direction');
+daspect([1 1]);
+subplot(1, 2, 2);
+image(cIy);
+title('Noise reduction y-direction');
+daspect([1 1]);
