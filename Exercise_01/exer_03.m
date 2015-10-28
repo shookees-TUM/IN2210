b6e1aed2a7f095947ef4a1a8efba0f9e34c654cd
Hanssteam <j.mahl@gmx.net>
2015-10-28 12:14:25 +0100

    added plot to show results
    for some reason matlab wants me to give three dimensions for keeping the
    aspect ratio. (daspect([1 1 1]);)

--- Exercise_01/exer_03.m
+++ Exercise_01/exer_03.m
@@ -1,15 +1,34 @@
 % exercise 3
-% a)
+% part a)
 % define basic grandient filters
 Dx = [-1, 0, 1; -1, 0, 1; -1, 0, 1];
 Dy = transpose(Dx);
 % Load image
 I = imread('lena.gif');
+colormap(gray(256)); 
+% Original image
+subplot(2, 3, 1);
+image(I);
+title('Original');
+daspect([1 1 1]); % Keeps aspect ratio 1:1
 % compute x gradient image
 Gradx = convoluteImage(Dx, I, 'mirror');
+% x gradient image
+subplot(2, 3, 2);
+image(Gradx);
+title('x gradient');
+daspect([1 1 1]); % Keeps aspect ratio 1:1
+%pause; 
 % compute y gradient image
 Grady = convoluteImage(Dy, I, 'mirror');
-% b)
+% x gradient image
+subplot(2, 3, 3);
+image(Grady);
+title('y gradient');
+daspect([1 1 1]); % Keeps aspect ratio 1:1
+%pause; 
+%
+% part b)
 % gradient magnitude
 grad_I = sqrt((Gradx)^2 + (Grady)^2);
 % gradient orientation
