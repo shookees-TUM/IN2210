9bc0cee09a8ec4d7c97289e9ce2f4f8436cf34e7
Hanssteam <j.mahl@gmx.net>
2015-10-29 11:11:58 +0100

    fix normalization in 1D Gaussian create

--- Exercise_01/gaussian1D.m
+++ Exercise_01/gaussian1D.m
@@ -4,6 +4,9 @@ function J = gaussian1D(mask_size, sigma)
     % Divide mask size by 2, so that 0 is the center
     s = floor(mask_size / 2);
     for u = -s: s
-        J(u + s + 1) = 1 / sqrt(2 * pi * sigma^2) * exp(-1 / 2 * (u^2) / (sigma^2));
+        J(u + s + 1) = exp(-1 / 2 * (u^2) / (sigma^2));
+        % prefactor 1 / sqrt(2 * pi * sigma^2) *
+        % not necessary because of normalization
     end
+J = 1 / sum(J) * J;
 end
