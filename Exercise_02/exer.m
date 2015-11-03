f69e5785e9b88fcd3d800f842cacb2586ce485b2
Paulius Šukys <paul.sukys@gmail.com>
2015-11-03 15:12:38 +0100

    Added noise functions. Not sure on gaussian one, used a solution from stackoverflow, need to investigate.

--- Exercise_02/exer.m
+++ Exercise_02/exer.m
@@ -2,7 +2,7 @@
 I = imread('lena.gif');
 filter_size = [5, 5];
 J = medianFilter(I, filter_size, 'multiply');
-figure('Name', 'Exercise 1');
+figure('Name', 'Exercise 1.a');
 colormap(gray(256)); 
 % Original image
 subplot(1, 2, 1);
@@ -14,3 +14,23 @@ image(J);
 title('Median Filtered');
 daspect([1 1]);
 %pause; 
+
+% noise takes on 10% of image (or in range [0..1] it's 0.1)
+noise_scale = 0.1;
+I_gaussian_noise = noise(I, 'gaussian', 0.1);
+I_saltpepr_noise = noise(I, 'saltandpepper', 0.1);
+figure('Name', 'Exercise 1.b');
+colormap(gray(256));
+subplot(1, 3, 1);
+image(I);
+title('Original');
+daspect([1 1]);
+subplot(1, 3, 2);
+image(I_gaussian_noise);
+title('Gaussian noise');
+daspect([1 1]);
+subplot(1, 3, 3);
+image(I_saltpepr_noise);
+title('Salt and pepper noise');
+daspect([1 1]);
+
