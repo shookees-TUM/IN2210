e9eda1e805a63d54706cd0b60b848e3f739c7f66
Hanssteam <j.mahl@gmx.net>
2015-11-04 13:32:53 +0100

    bilateral.m is working now
    adjusted class of padded image to be the same as class of input image to
    avoid problems (in addpadding.m)
    bilateral.m is working now. It's pretty slow compared to convolute with
    Gaussian for example.
    We have to think about how to implement even sigmas.
    I sent a mail to them asking if (in 2c) "bilateral range filter" really
    means omitting the domain part and only using the range part.

--- Exercise_02/bilateral.m
+++ Exercise_02/bilateral.m
@@ -1,10 +1,6 @@
 % Applies a bilateral filter to an Image (matrix) I
 % returning a filtered matrix J
 % border can be treated: "mirror" or "multiply"
-
-% (border treatment not implemented) maybe extract "padding.m" from
-% convolution.m ?
-
 % filter mask of size 3*sigma times 3*sigma pixels
 % central pixel = x ; neighboring pixel =  xi
 % desired amount of spatial smoothing: sigma_d
@@ -12,12 +8,14 @@
 function J = bilateral(I, sigma, sigma_d, sigma_r, border)
 % Determine size of image (for loop)
 image_size = size(I);
+% cast class of input image I
+I = double(I);
 % add padding to image
-size_kernel = (3*sigma, 3*sigma);
+size_kernel = [3*sigma, 3*sigma];
 padded_I = addpadding(I, size_kernel, border);
 % get start and stop index for for loop
 pad = floor(size_kernel / 2);
-
+%
 % generate domain smoothing matrix C(x, xi):
 % domain (smoothing) filter c(x, xi)
 % c = exp( -1/2 ( abs( xi - x ) / sigma_d )^2 )
@@ -26,22 +24,23 @@ for x = -pad(1):pad(1)
         distance(x+pad(1)+1, y+pad(2)+1) = sqrt(x^2 + y^2);
     end
 end
-C = exp( -1/2 ( distance / sigma_d )^2 );
-
+C = exp( -1/2 * ( distance / sigma_d ).^2 );
+% preallocate J for speed reasons
+J = zeros(image_size(1), image_size(2));
 % actual filtering process:
 % i and j refer to a position in the (padded) image
-for i = pad(1):(image_size(1) + pad(1))
-    for j = pad(2):(image_size(2) + pad(2))
-        % get matrix subsection around (i,j)
-        K = padded_I(i-pad(1):i+pad(1), j-pad(2):j+pad(2));
+for i = (pad(1)+1):(image_size(1) + pad(1))
+    for j = (pad(2)+1):(image_size(2) + pad(2))
+        % get matrix subsection [of size of kernel] around (i,j)
+        sub_I = padded_I(i-pad(1):i+pad(1), j-pad(2):j+pad(2));
         % generate range filter matrix S(x, xi)
         % range filter s(x, xi)
         % s = exp( -1/2 ( abs( I(xi) - I(x) ) / sigma_r )^2 )
-        abs_I_xi_I_x = sqrt( sum( ( K - I(i,j) * ones(pad(1), pad(2)) ).^2 ) );
-        S = exp( -1/2 ( abs_I_xi_I_x ) / sigma_r )^2 );
+        abs_I_xi_I_x=sqrt(sum(sum((sub_I-padded_I(i,j)*ones(size_kernel(1), size_kernel(2), 'like', padded_I)).^2)));
+        S = exp( -1/2 * ( abs_I_xi_I_x / sigma_r )^2 );
         % (see lecture 2 page 75 (of 77))
         % normalization missing!
-        J(i,j) = sum(K .* C .* S);
+        J(i,j) = 1 / sum( sum( C .* S ) ) * sum( sum(sub_I .* C .* S) );
     end
 end
 end
--- Exercise_02/exer.m
+++ Exercise_02/exer.m
@@ -13,9 +13,7 @@ subplot(1, 2, 2);
 image(J);
 title('Median Filtered');
 daspect([1 1]);
-%pause; 
-
-
+%pause;
 
 
 % Exercise 2
@@ -26,13 +24,14 @@ daspect([1 1]);
 % filter mask size: 3*sigma times 3*sigma pixels
 % central pixel = x ; neighboring pixel =  xi
 % desired amount of spatial smoothing:
-sigma_d = 10
+sigma_d = 10;
 % desired amount of combining of pixel values:
-sigma_r = 30
+sigma_r = 30;
 
-J_B1  = bilateral(I, 1, sigma_d, sigma_r)
-J_B5  = bilateral(I, 5, sigma_d, sigma_r)
-J_B10 = bilateral(I, 10, sigma_d, sigma_r)
+J_B1  = bilateral(I, 1, sigma_d, sigma_r, 'mirror');
+J_B5  = bilateral(I, 5, sigma_d, sigma_r, 'mirror');
+% IMPLEMENT EVEN SIGMAS!
+J_B10 = bilateral(I, 11, sigma_d, sigma_r, 'mirror');
 
 figure('Name', 'Exercise 2b)');
 colormap(gray(256)); 
@@ -57,16 +56,26 @@ image(J_B10);
 title('Bilateral filtered Image (sigma = 10)');
 daspect([1 1 1]); % Keeps aspect ratio 1:1
 %pause; 
-
+%
 % c) Compare bilateral range filter to normal Gaussian smoothing with sigma = 1, 5, 10
-
-J_BR1  =
-J_BR5  =
-J_BR10 =
-J_G1  =
-J_G5  =
-J_G10 =
-
+%
+% Is bilateral range filter different from bilateral filter?
+J_BR1  = J_B1;
+J_BR5  = J_B5;
+J_BR10 = J_B10;
+%
+% Create Gaussian kernels and convolute with image
+% mask_size = 3 * sigma;
+% G2D_sigma = gaussian2D(mask_size, sigma);
+G2D_sigma1 = gaussian2D(3, 1);
+J_G1 = convoluteImage(G2D_sigma1, I, 'mirror');
+G2D_sigma5 = gaussian2D(15, 5);
+J_G5 = convoluteImage(G2D_sigma5, I, 'mirror');
+% IMPLEMENT EVEN SIGMAS!
+G2D_sigma10 = gaussian2D(33, 11);
+J_G10 = convoluteImage(G2D_sigma10, I, 'mirror');
+%
+% Plot comparison of Gaussian and Bilateral Filtering
 figure('Name', 'Exercise 2c)');
 colormap(gray(256)); 
 % Bilateral Range filter with sigma = 1 applied
@@ -103,9 +112,9 @@ daspect([1 1 1]); % Keeps aspect ratio 1:1
 
 % d) Can you implement the bilateral filter with simple convolution masks? Why or why not?
 % No. Convolution is linear. Bilateral filter is non-linear.
+% Implementation-wise:  convolution has a constant kernel
+%                       range filter has a dynamic kernel (dependent on (i,j)[pixel position])
 
 % e) State the difference between domain filter and range filter
-% 
-
-
-
+% Both take a distance, but:    domain filter uses a spatial difference (constant)
+%                               range filter uses intensity difference (dynamic)
