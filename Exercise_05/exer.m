15cdd8a7ca0546bca48fa0e0fb46a40c89603735
Paulius Šukys <paul.sukys@gmail.com>
2015-11-25 11:35:30 +0100

    Implemented a slightly more flexible method, when the data folder, data file template and file numberings are passed as arguments to function

--- Exercise_05/exer.m
+++ Exercise_05/exer.m
@@ -1,2 +1,5 @@
 %%% Exercise 1 - loading the forest
-data_files = getDataFiles();
+data_folder = 'data/';
+data_file_template = 'Tree{0}.txt';
+data_file_numbers = linspace(0, 9, 10);
+data_files = getDataFiles(data_folder, data_file_template, data_file_numbers)
