2a6cedf3874cf2632414587aaee5309288f3f38b
Paulius Šukys <paul.sukys@gmail.com>
2015-11-26 16:21:25 +0100

    Added box evaluation function with feature test.

---
+++ Exercise_05/featureTest.m
@@ -0,0 +1,8 @@
+% returns true if < t; false if >= t
+function result = featureTest(x, y, node, img)
+    %    evaluateBox(x, y, xn, yn, zn, s, img)
+    b1 = evaluateBox(x, y, node.x(1), node.y(1), node.z(1), node.s, img);
+    b2 = evaluateBox(x, y, node.x(2), node.y(2), node.z(2), node.s, img);
+    f = b1 - b2;
+    result = f < node.t;
+end
